d328587ba3ae2c27ad5f62524a8821b4e0d73a89
stackarif <2017000000004@seu.edu.bd>
2022-09-12 22:11:29 +0600

    api develope of people table

--- app/Http/Controllers/Admin/Api/PeopleApiController.php
+++ app/Http/Controllers/Admin/Api/PeopleApiController.php
@@ -3,14 +3,19 @@
 namespace App\Http\Controllers\Admin\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Level_three;
 use App\Models\People;
 use Illuminate\Http\Request;
 
 class PeopleApiController extends Controller
 {
     public function people($slug){
-        $main_section=People::where('slug', $slug)->get();
-        
-        return response()->json($main_section);
+        $levelThree = Level_three::where('slug', $slug)->get();
+        if(count($levelThree) > 0){
+            $main_section= People::where('level_three_id',$levelThree[0]->id)->get();
+            return response()->json($main_section);
+        }else{
+            return response()->json($levelThree);
+        }
     }
 }
